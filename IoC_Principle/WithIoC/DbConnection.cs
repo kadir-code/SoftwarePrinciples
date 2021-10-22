8b65871c69ea316472313a8fe29342a5ba73ca04
kadir-code <65715423+xkadir@users.noreply.github.com>
2021-10-22 21:33:11 +0300

    DI Implementation

--- IoC_Principle/WithIoC/DbConnection.cs
+++ IoC_Principle/WithIoC/DbConnection.cs
@@ -14,6 +14,8 @@ namespace IoC_Principle
         //Now the helper class is designed to import the DbConnection object instance.
 
         //As we use the IConnection interface, we have prevented DbConnection from knowing about MongoDb and MsSQLDb classes.
+
+        /*
         private IConnection _connection;
         public DbConnection()
         {
@@ -23,6 +25,25 @@ namespace IoC_Principle
         public string GetData()
         {
             return _connection.GetData();
+        }*/
+
+        //After Dependency Injejtion
+        //There are 3 methods to inject, by property, method and constructor.
+        private IConnection _connection;
+        //Here we will give parameters to the constructor.
+        public DbConnection(IConnection connection)
+        {
+            _connection =connection;
         }
+
+        public string GetData()
+        {
+            return _connection.GetData();
+        }
+        /*Here, we have loosened the bond between the two classes a little more.
+         * If we want to convert our database source to MsSQLDb, 
+         * we need to do this by giving the MsSQLDbContext class 
+         * as a parameter where the object of the DbConnection class is created.
+         */
     }
 }
